b74212c8812354751388111c42b04ea747229724
Yasar icli <yasaricli@gmail.com>
2019-12-17 12:07:59 +0300

    fixed assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 const withLess = require('@zeit/next-less');
 
-module.exports = withLess();
+const isProduction = process.env.NODE_ENV === 'production';
+const PRODUCTION_STATIC_URL = 'https://yasaricli.github.io/';
+
+module.exports = {
+  assetPrefix: isProduction ? PRODUCTION_STATIC_URL : '',
+  ...withLess()
+}
